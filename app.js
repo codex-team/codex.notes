7e1bde4f1a1f88fdc53e8c9c832dcc09bee88f1c
Peter Savchenko <specc.dev@gmail.com>
2017-08-30 01:34:50 +0300

    Add error logging

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.log('\n\n\n', err);
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
