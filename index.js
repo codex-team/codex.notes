39db0eaa0de043d3ffe29ee7a88435d568d495c4
Peter Savchenko <specc.dev@gmail.com>
2017-08-31 00:29:59 +0300

    break no-space title

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.on('ready', function () {
   mainWindow.loadURL('http://localhost:3030');
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
     mainWindow = null;
