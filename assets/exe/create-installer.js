e87b837ae5dde53d03163f5f0803093e86848acc
Taly <vitalik7tv@yandex.ru>
2017-10-18 04:40:14 +0300

    Update packaging function

--- assets/exe/create-installer.js
+++ assets/exe/create-installer.js
@@ -3,10 +3,10 @@ var electronInstaller = require('electron-winstaller');
 let resultPromise = electronInstaller.createWindowsInstaller({
   appDirectory: 'release-builds/CodeX Notes-win32-x64',
   outputDirectory: 'release-builds/win-installer',
-  authors: 'My App Inc.',
+  authors: 'CodeX Team',
   exe: 'CodeX Notes.exe',
-  setupExe: 'CodeX Notes Installer',
-  setupIcon: 'assets/ico/icon-white128.ico'
+  setupExe: 'CodeX Notes Installer.exe',
+  setupIcon: 'assets/icons/ico/icon-white128.ico'
 });
 
 resultPromise.then(() => console.log('It worked!'), (e) => console.log(`No dice: ${e.message}`));
