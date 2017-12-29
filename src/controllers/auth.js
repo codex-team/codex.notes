5e0de109d955607a52d348594dce2316dd24c782
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-12-30 00:36:51 +0300

    Upd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -23,6 +23,11 @@ class AuthController {
   /**
    * Send auth request to Google api and get user`s profile information from server
    *
+   * 1. Open popup window with google oauth page
+   * 2. Google redirect to API server with oauth code
+   * 3. Server gets token and profile info and render page with JWT
+   * 4. Using WebContents Main Process gets JWT from popup
+   *
    * @param {Event} event — see {@link https://electronjs.org/docs/api/ipc-main#event-object}
    *
    */
@@ -35,16 +40,31 @@ class AuthController {
       }
     });
 
+    /**
+     * See {@link https://github.com/electron/electron/blob/master/docs/api/web-contents.md}
+     * @type {Electron.WebContents}
+     */
     let webContents = window.webContents;
 
-    webContents.on('did-finish-load', function (loadEvent) {
+    /** Fires when popup page is refreshed
+     *  @param {Event} loadEvent – onload event
+     */
+    webContents.on('did-finish-load', loadEvent => {
+      /**
+       * Page with JWT should be loaded second
+       */
       if (loadEvent.sender.currentIndex < 1) return;
 
-      webContents.executeJavaScript('document.getElementById("jwt").textContent')
+      /**
+       * Just get content from div with "jwt" id, contains user`s JWT
+       */
+      webContents.executeJavaScript('document.body.textContent')
         .then(function (jwt) {
           try {
+            /** Decode JWT payload */
             let payload = new Buffer(jwt.split('.')[1], 'base64');
 
+            /** Try to parse payload as JSON. If this step fails, it means that auth failed at all */
             payload = JSON.parse(payload);
 
             event.returnValue = {
@@ -54,11 +74,18 @@ class AuthController {
               token: jwt
             };
           } catch(e) {
+            console.log('Google OAuth failed because of ', e);
             event.returnValue = false;
           }
 
           window.close();
         });
+    });
+
+    window.on('closed', () => {
+      if (event.returnValue === undefined) {
+        event.returnValue = false;
+      }
     });
 
     window.loadURL('https://accounts.google.com/o/oauth2/v2/auth?' +
