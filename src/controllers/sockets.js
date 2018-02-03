0acefa2acb95e3dcc6d765d81376e0f3fb77d32c
Petr Savchenko <specc.dev@gmail.com>
2018-02-04 02:19:30 +0300

    new auth scheme: accept JWT by socket

--- src/controllers/sockets.js
+++ src/controllers/sockets.js
@@ -5,34 +5,56 @@ const WebSocket = require('ws');
 class Sockets {
   constructor(){
     this.listenedChannels = [];
-
   }
 
   /**
    * Starts to listen passed channel
    * @param channel
    */
-  listenChannel(channel){
-    this.listenedChannels.push(new Channel(channel));
+  listenChannel(channel, callback){
+    this.listenedChannels.push(new Channel(channel, callback));
   }
 }
 
 class Channel {
-  constructor(name){
+  constructor(name, callback){
 
-    let channelULR = 'ws://localhost:8081/chan/' + name;// + '.b10'
-    this.ws = new WebSocket(channelULR);
-
-    console.log('trying to listen ' + name);
-    this.ws.on('open', () => {
-      console.log('listenign channel ' + name);
-      // this.ws.send('listenting...');
+    this.name = name;
+    this.callback = callback;
+    this.url = 'ws://localhost:8081/chan/' + this.name;// + '.b10'
+    this.ws = new WebSocket(this.url, {
+      /** Disable a negotiate a compression algorithm {@link https://github.com/websockets/ws#websocket-compression} */
+      perMessageDeflate: false
     });
 
-    this.ws.on('message', (data) => {
+    console.log('trying to listen ' + this.name);
+
+    this.ws.on('open', () => {
+      this.opened();
+    });
+
+    this.ws.on('close', () => {
+      this.closed();
+    });
+
+    this.ws.on('message', data => {
+      this.onMessage(data);
     });
 
   }
+
+  opened(){
+    console.log('listening channel ' + this.name);
+  }
+
+  closed(){
+    console.log('disconnected from ' + this.name);
+  }
+
+  onMessage(data){
+    this.callback(data);
+  }
+
 }
 
 module.exports = Sockets;
