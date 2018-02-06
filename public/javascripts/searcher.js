6306122c833e8b5abebebd8a2e24b518db2d7def
Илья Колдунов <gigafiga21@gmail.com>
2018-02-06 19:02:50 +0200

    Added search algorithm (issue #133).

--- public/javascripts/searcher.js
+++ public/javascripts/searcher.js
@@ -12,15 +12,35 @@ export default class Searcher {
       serchField: null
     };
 
+    /**
+     * Where to search
+     * @type {Array}
+     */
     this.dataset = null;
   }
 
-    /**
-     * Set data where search will be done
-     * @param {Array} data - array to set
-     */
+  /**
+   * Set data where search will be done
+   * @param {Array} data - array to set
+   */
   set data( data ) {
     this.dataset = data;
+  }
+
+  /**
+   * Set data where search will be done
+   * @param {Array} data - array to set
+   */
+  search( item ) {
+    let found = [];
+
+    this.dataset.forEach((element) => {
+      if (element.indexOf(item) == 0)      {
+        found.push(element);
+      }
+    });
+
+    return found;
   }
 
 }
