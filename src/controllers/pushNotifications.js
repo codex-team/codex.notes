6993281eca513efed28699c1bf6a68f7b3145fa3
Murod Khaydarov <murod.haydarov@gmail.com>
2018-04-09 15:01:26 +0300

    using node-notifier instead of native due to the cross-platform support

--- src/controllers/pushNotifications.js
+++ src/controllers/pushNotifications.js
@@ -5,20 +5,31 @@
  */
 
 /**
- * Using native Election Push notifications
- * https://electronjs.org/docs/api/notification
- *
- * @type {Electron.Notification}
+ * Using node-notifier library to support all Windows, Linus and others
+ * https://github.com/mikaelbr/node-notifier/blob/master/DECISION_FLOW.md
  */
-const { Notification } = require('electron');
+const notifier = require('node-notifier');
+
+/**
+ * Path library
+ * @type {"path"}
+ */
+const path = require('path');
 
 /**
  * @typedef {Object} PushOptions
- * @property {String} title - information that will be shown at the top of the notification
- * @property {String} subtitle - small information that will be shown at the bottom
- * @property {String} body - notification content
+ * @property {String} title - Message title. Will be appeared as push subtitle
+ * @property {String} message - notification content
  * @property {Boolean} silent - need sound or not
- * @property {String} icon - notification icon. Using CodeX Notes logo as default
+ * @property {String} image - notification icon. Using CodeX Notes logo as default
+ *
+ */
+
+/**
+ * @typedef {Object} NotifierOptions
+ * @property {String} title  - information that will be shown at the top of the notification
+ * @property {String} subtitle - small information that will be shown at the bottom
+ * @property {String} message - notification text content
  * @property {String} sound - the name of file to play when the notification is shown
  */
 
@@ -30,7 +41,10 @@ class PushNotifications {
     /**
      * @constructor
      */
-    constructor() { }
+    constructor() {
+        this.title = "CodeX Notes";
+        this.icon = path.join(__dirname, '/../../assets/icons/png/icon-white512.png');
+    }
 
     /**
      * sends push notification to the main proceess
@@ -39,17 +53,19 @@ class PushNotifications {
      */
     send(options) {
 
-        if ( !Notification.isSupported() ) {
-            return false;
-        }
+        /**
+         * Assemble data from passed options
+         * @type {NotifierOptions}
+         */
+        let notifierOption = {
+            icon : this.icon,
+            title : this.title,
+            subtitle : options.title,
+            contentImage : options.image,
+            message : options.message,
+        };
 
-        options.icon = options.icon || 'default';
-        options.sound = 'path to file';
-        options.silent = options.silent || false;
-
-        let notification = new Notification(options);
-        notification.show();
-
+        notifier.notify(notifierOption);
     }
 
 }
