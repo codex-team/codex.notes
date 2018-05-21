8ed4f361b4742b8d3f7a078c608c2354fa521f8b
Taly <vitalik7tv@yandex.ru>
2018-05-21 21:47:39 +0300

    Add comments to env

--- env.sample.js
+++ env.sample.js
@@ -12,14 +12,15 @@ let vars = {
 
   GOOGLE_CLIENT_ID: '',
 
+  /** Production Codex Notes API Server */
   GOOGLE_REDIRECT_URI: 'https://api.notes.ifmo.su/oauth/code',
   API_ENDPOINT: 'https://api.notes.ifmo.su/graphql',
   REALTIME_DAEMON: 'wss://api.notes.ifmo.su:22002',
 
-// LOCAL DEV
-// GOOGLE_REDIRECT_URI: 'http://localhost:8081/oauth/code',
-// API_ENDPOINT: 'http://localhost:8081/graphql',
-// REALTIME_DAEMON: 'ws://localhost:8081',
+  /** Local CodeX Notes API Server */
+  // GOOGLE_REDIRECT_URI: 'http://localhost:8081/oauth/code',
+  // API_ENDPOINT: 'http://localhost:8081/graphql',
+  // REALTIME_DAEMON: 'ws://localhost:8081',
 };
 
 for (let property in vars) {
